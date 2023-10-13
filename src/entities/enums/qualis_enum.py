10bc9cd85e4d60df1c62053f55acd0685b8a1c25
Matheus Moura <matheus.dsmoura@protonmail.com>
2023-10-13 16:06:16 -0300

    Turning QualisEnum into an iterable.

--- src/entities/enums/qualis_enum.py
+++ src/entities/enums/qualis_enum.py
@@ -4,15 +4,17 @@ from enum import Enum
 class QualisEnum(Enum):
     """Represents each qualis classification of Scientific Serial Publishers."""
 
-    NOT_CLASSIFIED = "NC"
-    A1 = "A1"
-    A2 = "A2"
-    A3 = "A3"
-    A4 = "A4"
-    A5 = "A5"
-    B1 = "B1"
-    B2 = "B2"
-    B3 = "B3"
-    B4 = "B4"
-    B5 = "B5"
-    C = "C"
+    __order__ = 'A1 A2 A3 A4 A5 B1 B2 B3 B4 B5 C NOT_CLASSIFIED'
+
+    A1 = 'A1'
+    A2 = 'A2'
+    A3 = 'A3'
+    A4 = 'A4'
+    A5 = 'A5'
+    B1 = 'B1'
+    B2 = 'B2'
+    B3 = 'B3'
+    B4 = 'B4'
+    B5 = 'B5'
+    C = 'C'
+    NOT_CLASSIFIED = 'NC'
